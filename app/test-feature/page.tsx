65367208973f1cc4c1ebe93df6e19bf3a8428a36
mrpoffice <mrpoffice@gmail.com>
2025-11-08 12:17:02 -0600

    Test: dummy feature for git hook

---
+++ app/test-feature/page.tsx
@@ -0,0 +1,10 @@
+// Dummy page to test Glow Guide git hook
+
+export default function TestFeature() {
+  return (
+    <div>
+      <h1>Test Feature</h1>
+      <p>This is a test page to trigger the Glow Guide detection hook.</p>
+    </div>
+  )
+}
